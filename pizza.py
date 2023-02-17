49ea0e48209aebc704d04ed28a0a9ff4d5ba2439
kaitlynnbeston1 <113719685+kaitlynnbeston1@users.noreply.github.com>
2023-02-17 09:13:57 -0600

    Init

---
+++ pizza.py
@@ -0,0 +1,11 @@
+# Creating the list
+pizzas = ["vegetarian", "cheese", "ham and pineapple"]
+
+
+# For loop
+for pizza in pizzas:
+    print(f"I really like {pizza} pizza.")
+
+    
+    # print statement
+    print("All of these pizzas are amazing, so it's hard to choose just one favorite.")
